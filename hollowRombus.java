3cef32639d234023fc4a54c91d3e271308c396e3
Mohd-Shams <mohdshamsm20@gmail.com>
2026-01-20 21:29:20 +0530

    added basic patterns

---
+++ hollowRombus.java
@@ -0,0 +1,23 @@
+public class hollowRombus {
+     public static void main (String args[]){
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=9;j++)
+            {
+                if ((i+j)>=6 && (i+j)<=10)
+                    {
+                    if(i==1 || i==5|| i+j==6||i+j==10)
+                        System.out.print("*");
+                        else
+                        System.out.print(" ");
+                    }
+                    else
+                        System.out.print(" ");
+                         
+            }
+        System.out.println();
+
+        }
+    }
+    
+}
